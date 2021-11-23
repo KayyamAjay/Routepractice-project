9345062dc6d57ac573684daf7fba2cdd1a51520e
Ajay Kayyam <saiajay220@gmail.com>
2021-11-24 00:12:14 +0530

    adding NavBar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetails from "./pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteid">
-        <QuoteDetails />
-      </Route>
-      <Route path="/newquote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteid">
+          <QuoteDetails />
+        </Route>
+        <Route path="/newquote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
