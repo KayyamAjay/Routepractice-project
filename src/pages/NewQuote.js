fb30310f3419b7e1a2ed9b7b2e2b6951d88c4c57
Ajay Kayyam <saiajay220@gmail.com>
2021-11-25 11:43:50 +0530

    Adding promt

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,10 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router";
 const NewQuote = () => {
+  const history = useHistory();
   const addHandler = (data) => {
     console.log(data);
+    history.push("/quotes"); //gets to quotes page after submiting
   };
   return <QuoteForm onAddQuote={addHandler} />;
 };
